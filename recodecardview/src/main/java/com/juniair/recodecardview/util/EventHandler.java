5f044b6260540b0df109cfd93c9e0a5b801f40e7
KimKyengJun <juniair@naver.com>
2016-08-03 18:47:43 +0900

    Inheritance PopupMenu.OnMenuItemClickListener

--- recodecardview/src/main/java/com/juniair/recodecardview/util/EventHandler.java
+++ recodecardview/src/main/java/com/juniair/recodecardview/util/EventHandler.java
@@ -1,7 +1,9 @@
 package com.juniair.recodecardview.util;
 
+import android.support.v7.widget.PopupMenu;
+
 /**
  * Created by juniair on 2016-08-03.
  */
-public interface EventHandler {
+public interface EventHandler extends PopupMenu.OnMenuItemClickListener{
 }
